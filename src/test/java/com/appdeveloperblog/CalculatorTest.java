4e9bbbf1c00218589ba1a54f2a0e0c241ffbe9d3
Steven.Huang <steven.huang@hy-tech.com.tw>
2022-11-14 18:36:42 +0800

    增加註解

--- src/test/java/com/appdeveloperblog/CalculatorTest.java
+++ src/test/java/com/appdeveloperblog/CalculatorTest.java
@@ -9,22 +9,26 @@ class CalculatorTest {
     Calculator calculator;
 
     @BeforeAll
+    // 方法要為static，有此註解的方法會在所有Test Methods執行前執行。
     static void setup() {
         System.out.println("BeforeAll is executed");
     }
 
     @BeforeEach
+    //有此註解的方法會在當前Class中的所有Test Methods執行前執行。
     void beforeEachTestMethod() {
         calculator = new Calculator();
         System.out.println("BeforeEach is executed");
     }
 
     @AfterEach
+    //有此註解的方法會在當前Class中的所有Test Methods執行後執行。
     void afterEachTestMethod() {
         System.out.println("AfterEach is executed");
     }
 
     @AfterAll
+    // 有此註解的方法要為static，有此註解的方法會在所有Test Methods執行後執行。
     static void afterAll() {
         System.out.println("AfterAll is executed");
     }
