c369ae0f9cc4e84f33f8a5aa8e55f50c1914aeb7
Steven.Huang <steven.huang@hy-tech.com.tw>
2022-12-05 17:54:22 +0800

    @ParameterizedTest. Multiple Parameters with @MethodSource.

--- src/test/java/com/appdeveloperblog/CalculatorTest.java
+++ src/test/java/com/appdeveloperblog/CalculatorTest.java
@@ -1,6 +1,12 @@
 package com.appdeveloperblog;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Test Methods in Calculator class")
@@ -64,5 +70,27 @@ class CalculatorTest {
         }, "沒噴錯");
             // assert
         assertEquals(expectedExceptionMessage,actualException.getMessage(),"Unexpected error message");
+    }
+
+    @DisplayName("Test integer subtraction [minuend, subtrahend, expectedResult")
+    @ParameterizedTest
+    @MethodSource("integerSubtractionInputParameters")
+    // 參數化測試使使用不同參數多次進行測試成為可能。它們與常規的@Test方法一樣被宣告，但是使用 @ParameterizedTest註解。
+    // 此外，您必須聲明至少一個源，該源將為每個調用提供參數，然後使用測試方法中的參數，即使用 @MethodSource("方法名稱")。
+    // 如果MethodSource的來源名稱和此測試程式名稱一樣，可以省略不寫。
+    // i.e, 將integerSubtractionInputParameters改名成integerSubtraction後，註解可以改寫成 @MethodSource()
+    void integerSubtraction(int minuend, int subtrahend, int expectedResult) {
+        System.out.println("Running Test "+minuend+" - "+subtrahend+" = "+expectedResult);
+        int actualResult = calculator.integerSubtraction(minuend, subtrahend);
+        assertEquals(expectedResult, actualResult,
+                ()->minuend + "-" + subtrahend + "did not produce" + expectedResult);
+    }
+
+    private static Stream<Arguments> integerSubtractionInputParameters() {
+         return Stream.of(
+                 Arguments.of(33, 1, 32),
+                 Arguments.of(24, 1, 23),
+                 Arguments.of(54, 1, 53)
+         );
     }
 }
